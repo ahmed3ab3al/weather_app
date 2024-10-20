c7cbafc646d0659357eeb22b207cd6366ef64a70
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-10-20 22:58:00 +0300

    Create Dio Services

---
+++ lib/core/api/end_points.dart
@@ -0,0 +1,12 @@
+class EndPoints {
+  static const String baseUrl = 'http://api.weatherapi.com/v1';
+  static const String search = '/forecast.json';
+  static const String apiKey = '5e6ed2285b65436a9f813120231408';
+}
+
+class ApiKeys {
+  static const String searchKey = 'q';
+  static const String apiKey = 'key';
+  static const String status = 'statusCode';
+  static const String errMessage = 'error';
+}
