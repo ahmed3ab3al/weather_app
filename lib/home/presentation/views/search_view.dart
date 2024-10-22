a9e2600459a54e7db038d9086f38f506a2b48772
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-10-22 16:15:48 +0300

    handel search weather

--- lib/home/presentation/views/search_view.dart
+++ lib/home/presentation/views/search_view.dart
@@ -1,38 +1,47 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather/home/presentation/view_models/weather_cubit/weather_cubit.dart';
+import 'package:weather/home/presentation/view_models/weather_cubit/weather_states.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Search city'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Center(
-          child: TextField(
-            onSubmitted: (value) async {
-            },
-            decoration: InputDecoration(
-                contentPadding:
-                const EdgeInsets.symmetric(vertical: 24, horizontal: 20),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
+    return BlocConsumer<WeatherCubit, WeatherStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Search city'),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Center(
+                child: TextField(
+                  onSubmitted: (value) async {
+                    WeatherCubit.get(context).getWeather(cityName: value);
+                    GoRouter.of(context).pop();
+                  },
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 24, horizontal: 20),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide:
+                              const BorderSide(color: Colors.blueAccent)),
+                      suffixIcon: const Icon(Icons.search),
+                      labelText: 'Search'),
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: const BorderSide(color: Colors.blueAccent)),
-                suffixIcon: const Icon(Icons.search),
-                labelText: 'Search'),
-          ),
-        ),
-      ),
-    );
+              ),
+            ),
+          );
+        });
   }
 }
