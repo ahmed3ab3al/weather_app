bdb62648a3bca79117ead7f093d8a3613c5ab7eb
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-10-22 16:28:08 +0300

    extract weather Data from model

--- lib/home/presentation/views/widgets/weather_info.dart
+++ lib/home/presentation/views/widgets/weather_info.dart
@@ -1,57 +1,65 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather/home/data/models/weather_model.dart';
+import 'package:weather/home/presentation/view_models/weather_cubit/weather_cubit.dart';
 
 class WeatherInfoBody extends StatelessWidget {
-  const WeatherInfoBody({super.key,});
+  const WeatherInfoBody({super.key, required this.weatherModel});
 
+  final WeatherModel weatherModel;
   @override
   Widget build(BuildContext context) {
-    return  const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('cityName',
-                style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold
-                ),),
-              Text('update at',style: TextStyle(
-                fontSize: 24,
-              ),),
-              SizedBox(height: 32,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Image(image: NetworkImage(''),),
-                  Text('temp',
-                    style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold
-                    ),),
-                  Column(
-                    children: [
-                      Text('MaxTemp:',style:
-                      TextStyle(
-                          fontSize: 16
-                      ),),
-                      Text('MinTemp: ',style:
-                      TextStyle(
-                          fontSize: 16
-                      ),)
-                    ],
-                  )
-                ],
-              ),
-              SizedBox(height: 16,),
-              Text('weatherCondition',
-                style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold
-                ),
-              ),
-            ]
+    WeatherModel weatherModel =
+        BlocProvider.of<WeatherCubit>(context).weatherModel!;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text(
+          weatherModel.cityName,
+          style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
         ),
-      );
+        Text(
+          'update at ${weatherModel.date.hour}:${weatherModel.date.minute}',
+          style: const TextStyle(
+            fontSize: 24,
+          ),
+        ),
+        const SizedBox(
+          height: 32,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image(
+              image: NetworkImage('https:${weatherModel.image}'),
+            ),
+            Text(
+              weatherModel.temp.toString(),
+              style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            Column(
+              children: [
+                Text(
+                  'MaxTemp: ${weatherModel.maxTemp.round()}',
+                  style: const TextStyle(fontSize: 16),
+                ),
+                Text(
+                  'MinTemp: ${weatherModel.minTemp.round()}',
+                  style: const TextStyle(fontSize: 16),
+                )
+              ],
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Text(
+          weatherModel.weatherCondition,
+          style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        ),
+      ]),
+    );
   }
 }
